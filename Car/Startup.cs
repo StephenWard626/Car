32fa93cc6354ec1ca15e084d66f80535c7ca4763
StephenWard626 <78171486+StephenWard626@users.noreply.github.com>
2021-03-18 20:03:51 +0000

    Changed Startup page

--- Car/Startup.cs
+++ Car/Startup.cs
@@ -50,7 +50,7 @@ namespace Car
             {
                 endpoints.MapControllerRoute(
                     name: "default",
-                    pattern: "{controller=Home}/{action=Index}/{id?}");
+                    pattern: "{controller=Car}/{action=Index}/{id?}");
             });
         }
     }
